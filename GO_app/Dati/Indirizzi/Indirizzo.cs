44d6dcd6ac9dbb7eccec00582214c90940818488
Luca Giobini <99609468+LucaGiob@users.noreply.github.com>
2025-12-25 22:10:35 +0100

    Creazione classi indirizzo e piano

---
+++ GO_app/Dati/Indirizzi/Indirizzo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GO_app.Dati.Indirizzi
+{
+    internal class Indirizzo
+    {
+        public char id;
+        public string nome;
+
+        public List<Piano> piani;
+
+        public Indirizzo()
+        {
+            nome = string.Empty;
+            piani = [];
+        }
+    }
+}
---
+++ GO_app/Dati/Indirizzi/Piano.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GO_app.Dati.Indirizzi
+{
+    internal class Piano
+    {
+        public int yr1;
+        public int yr2;
+        public int yr3;
+        public int yr4;
+        public int yr5;
+
+        public Piano() { }
+    }
+}
