8173834ae6d337faa60ff49430d54573446202e3
Luca Giobini <99609468+LucaGiob@users.noreply.github.com>
2025-12-25 22:18:57 +0100

    Creazione classe progetto

---
+++ GO_app/Dati/Progetto.cs
@@ -0,0 +1,22 @@
+﻿using GO_app.Dati.Indirizzi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GO_app.Dati
+{
+    internal class Progetto
+    {
+        public string nome;
+
+        public List<Indirizzo> indirizzi;
+
+        public Progetto()
+        {
+            nome = string.Empty;
+            indirizzi = [];
+        }
+    }
+}
