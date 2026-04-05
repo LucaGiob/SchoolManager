0158802e2797f46a91ec1c13f85910e352433819
Luca Giobini <99609468+LucaGiob@users.noreply.github.com>
2026-04-05 19:25:51 +0200

    Creating the block class

---
+++ SM_app/Table/Blocks/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM_app.Table.Blocks
+{
+    internal class Block
+    {
+        internal string Classe {  get; set; }
+        internal string Materia { get; set; }
+        internal string Professore { get; set; }
+
+        internal Block(string classe, string materia, string professore)
+        {
+            Classe = classe;
+            Materia = materia;
+            Professore = professore;
+        }
+    }
+}
